a1c6c0a44fae6b52935f16bce0f84c75ce2bc901
pushpabhat55 <pushpabhat55@gmail.com>
2025-03-28 20:34:50 +0530

    not found and error component

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Page not found</h1>
+        <p>unfortunately, couldn't find the requested page</p>
+    </main>
+}
